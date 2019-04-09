1ffa92eec3e01e65754571da4bd8b58711df5e35
monokh <mnokhb@gmail.com>
2019-04-09 09:17:35 +0100

    Remove unnecessary files

--- webpack/plugins.js
+++ webpack/plugins.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const webpack = require('webpack')
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
 
 module.exports = (config = { target: 'web' }) => {
@@ -18,18 +17,6 @@ module.exports = (config = { target: 'web' }) => {
         }
       })
     )
-
-    if (config.target === 'web') {
-      plugins.push(
-        new BundleAnalyzerPlugin({
-          analyzerMode: 'static',
-          reportFilename: path.resolve(__dirname, '..', 'docs', 'bundle', 'index.html'),
-          openAnalyzer: false,
-          generateStatsFile: true,
-          statsFilename: path.resolve(__dirname, '..', 'docs', 'bundle', 'stats.json')
-        })
-      )
-    }
   }
 
   return plugins
