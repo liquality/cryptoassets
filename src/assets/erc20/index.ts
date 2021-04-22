02e1cc2174972fde4c2ec509458a6e42b1b563a2
Harsh Vakharia <harshjv@gmail.com>
2021-04-22 15:46:37 +0530

    destructure mapValues

--- src/assets/erc20/index.ts
+++ src/assets/erc20/index.ts
@@ -1,19 +1,19 @@
-import _ from 'lodash'
+import { mapValues } from 'lodash'
 import { Asset, ChainId, AssetType } from '../../types'
 import ethereumTokens from './ethereum-tokens.json'
 import rskTokens from './rsk-tokens.json'
 
-const rskTokensData = _.mapValues(rskTokens, (tokenData) => ({
+const rskTokensData = mapValues(rskTokens, (tokenData) => ({
   ...tokenData,
   chain: ChainId.Rootstock
 }))
 
-const ethereumTokensData = _.mapValues(ethereumTokens, (tokenData) => ({
+const ethereumTokensData = mapValues(ethereumTokens, (tokenData) => ({
   ...tokenData,
   chain: ChainId.Ethereum
 }))
 
-const tokens: { [index: string]: Asset } = _.mapValues({ ...rskTokensData, ...ethereumTokensData }, (tokenData) => ({
+const tokens: { [index: string]: Asset } = mapValues({ ...rskTokensData, ...ethereumTokensData }, (tokenData) => ({
   ...tokenData,
   type: 'erc20' as AssetType
 }))
