1a4c2fe3fd30651bfc264b83eb8a4258070f2734
Raj Beemi <beemi.raja@gmail.com>
2022-02-07 19:10:37 +0000

    test: fix some avalance tests

--- tests/chains/avalanche.test.ts
+++ tests/chains/avalanche.test.ts
@@ -25,7 +25,13 @@ describe('Avalanche chain tests', function () {
   })
 
   it('Provides correct address validation', () => {
-    const validAddress = ['avax10nfrs8ja5sg44664s302y5rany28zmy0s48d86', 'avax1k30tskunzxr2tmapy8p4y0ujn2802yr3743679']
+    const validAddress = [
+      '0xB27515Ef1B667493eAfc48DA244a1fD7549eaeB6',
+      '0xd3ff7ba8d4E2d4D075172938D60a2fCd7d299aC1',
+      '0x39912D83acb4A373321387300f4FBE88Aa5d6F14',
+      '0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E',
+      '0x60aE616a2155Ee3d9A68541Ba4544862310933d4'
+    ]
 
     validAddress.forEach((a: string) => {
       expect(avalanche.isValidAddress(a)).to.be.true
@@ -38,7 +44,13 @@ describe('Avalanche chain tests', function () {
   })
 
   it('Provides correct address formatting', () => {
-    const address = ['fuji1kvg7f6ppmkdu7w2frckh29ttsz7287nejkmlyc']
+    const address = [
+      '0xB27515Ef1B667493eAfc48DA244a1fD7549eaeB6',
+      '0xd3ff7ba8d4E2d4D075172938D60a2fCd7d299aC1',
+      '0x39912D83acb4A373321387300f4FBE88Aa5d6F14',
+      '0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E',
+      '0x60aE616a2155Ee3d9A68541Ba4544862310933d4'
+    ]
 
     address.forEach((a: string, index: number) => {
       expect(avalanche.formatAddress(a)).to.be.equal(address[index])
@@ -46,8 +58,7 @@ describe('Avalanche chain tests', function () {
   })
 
   it('Provides correct tx validation', () => {
-    const validTxHash =
-      '8pRdygzR8Zk9oMYmqiyzUpqJQnFmaAnMLniexycNLavr_9eed84cfc2ac0068dd8fc10b8b3b71c8d0f74cfd09211e036bdb8561c2647472'
+    const validTxHash = '0xb4ec649426ba04cd0407d5929288ff3d59d8baca9b810cf4147620bab6dadba3'
     expect(avalanche.isValidTransactionHash(validTxHash)).to.be.true
 
     const validTxHashInvalidSigner =
@@ -64,8 +75,7 @@ describe('Avalanche chain tests', function () {
   })
 
   it('Provides correct tx formatting', () => {
-    const txHash =
-      '8pRdygzR8Zk9oMYmqiyzUpqJQnFmaAnMLniexycNLavr_9eed84cfc2ac0068dd8fc10b8b3b71c8d0f74cfd09211e036bdb8561c2647472'
+    const txHash = '0xb4ec649426ba04cd0407d5929288ff3d59d8baca9b810cf4147620bab6dadba3'
     expect(avalanche.formatTransactionHash(txHash)).to.be.equal(txHash)
   })
 })
